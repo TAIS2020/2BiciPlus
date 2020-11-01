eebdc80eeeb36b1f974dc4ced218c0e73c2d655f
charlie2634 <43752006+charlie2634@users.noreply.github.com>
2020-11-01 10:05:06 -0500

    Initial Commit

---
+++ backend/src/main/java/com/biciplus/backend/BiciplusbackendApplication.java
@@ -0,0 +1,13 @@
+package com.biciplus.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiciplusbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiciplusbackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/biciplus/backend/BiciplusbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biciplus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiciplusbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
