c0f8d029385cf8d39fa54ce161034d45df3ebdd2
Henry Salazar <henrysteven003@gmail.com>
2020-11-12 15:11:20 -0500

    dashboard terminado view y carrito de compras

--- FRONTEND/twobiciplus/src/app/views/dashboard/dashboard.component.ts
+++ FRONTEND/twobiciplus/src/app/views/dashboard/dashboard.component.ts
@@ -12,12 +12,42 @@ export class DashboardComponent implements OnInit {
       name: 'Silla',
       description: 'Para sentarse bien',
       price: '$500000',
-      image: 'https://contents.mediadecathlon.com/p1133140/k$933301a2f95f07c3e68f4ba3078635df/sillin-bicicleta-urbana-100.jpg?format=auto&f=400x400'
+      image: 'https://contents.mediadecathlon.com/p1133140/k$933301a2f95f07c3e68f4ba3078635df/sillin-bicicleta-urbana-100.jpg?format=auto&f=400x400',
+      id: 123
+    },
+    {
+      name: 'Silla',
+      description: 'Para sentarse bien',
+      price: '$500000',
+      image: 'https://contents.mediadecathlon.com/p1133140/k$933301a2f95f07c3e68f4ba3078635df/sillin-bicicleta-urbana-100.jpg?format=auto&f=400x400',
+      id: 123
+    },
+    {
+      name: 'Silla',
+      description: 'Para sentarse bien',
+      price: '$500000',
+      image: 'https://contents.mediadecathlon.com/p1133140/k$933301a2f95f07c3e68f4ba3078635df/sillin-bicicleta-urbana-100.jpg?format=auto&f=400x400',
+      id: 123
+    },
+    {
+      name: 'Silla',
+      description: 'Para sentarse bien',
+      price: '$500000',
+      image: 'https://contents.mediadecathlon.com/p1133140/k$933301a2f95f07c3e68f4ba3078635df/sillin-bicicleta-urbana-100.jpg?format=auto&f=400x400',
+      id: 123
     }
   ];
+
+  cartArticles = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addCart(product: any) {
+    //add to cart in back
+    console.log(product);
+    this.cartArticles++;
+    
+  }
 }
