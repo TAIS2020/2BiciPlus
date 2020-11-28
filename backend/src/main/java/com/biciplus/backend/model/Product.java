851a5de52bddf1b27e6f32ba022d567e8bee1f46
charlie2634 <43752006+charlie2634@users.noreply.github.com>
2020-11-28 10:30:38 -0500

    Changes in model, now it is only used 1 category per product

--- backend/src/main/java/com/biciplus/backend/model/Product.java
+++ backend/src/main/java/com/biciplus/backend/model/Product.java
@@ -1,7 +1,5 @@
 package com.biciplus.backend.model;
 
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,31 +8,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.JoinColumn;
-
+import javax.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes({ 
-	@JsonSubTypes.Type(value = SimpleProduct.class, name = "simple"),
-	@JsonSubTypes.Type(value = ComposedProduct.class, name = "composed") 
-	})
+@JsonSubTypes({ @JsonSubTypes.Type(value = SimpleProduct.class, name = "simple"),
+		@JsonSubTypes.Type(value = ComposedProduct.class, name = "composed") })
 public abstract class Product {
-	@Id   
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Long id;
 
-	@ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
-	@JoinTable(name = "product_categories", 
-	    joinColumns = @JoinColumn(name="product_id"),
-	    inverseJoinColumns = @JoinColumn(name="categories_id"))
-	protected Set<Category> categories;
-	
+	@ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+	protected Category category;
+
 	public Long getId() {
 		return id;
 	}
@@ -43,14 +33,15 @@ public abstract class Product {
 		this.id = id;
 	}
 
-	public Set<Category> getCategories() {
-		return categories;
+	public Category getCategory() {
+		return category;
 	}
 
-	public void setCategories(Set<Category> categories) {
-		this.categories = categories;
+	public void setCategory(Category category) {
+		this.category = category;
 	}
 
 	public abstract Long getPrice();
+
 	public abstract String getType();
 }
