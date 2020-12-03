61097c04e5c49a15dae4ba2ab14b30999d16ed35
edjuarcu <72586768+edjuarcu@users.noreply.github.com>
2020-12-03 05:41:04 -0500

    Se Actualiza la clase Composed Report

--- backend/src/main/java/com/biciplus/backend/model/ComposedReport.java
+++ backend/src/main/java/com/biciplus/backend/model/ComposedReport.java
@@ -1,5 +1,53 @@
 package com.biciplus.backend.model;
 
 public class ComposedReport extends Report {
+	private String id;
+	private String name;
+	private String price;
+	private String details;
+	private String cname;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}	
+	
+	public void setPrice(String price) {
+		this.price = price;
+		
+	}
+	
+	public String getDetails() {
+		return details;
+	}	
+	
+	public void setDetails(String details) {
+		this.details = details;
+		
+	}
+	
+	public String getCname() {
+		return cname;
+	}	
+	
+	public void setCname(String cname) {
+		this.cname = cname;
+		
+	}
+	
 }
